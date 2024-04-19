0991e27adb501310956525fb3ce6c14563c7b649
Joshua Meza <joshuameza1@gmail.com>
2024-04-19 00:08:29 -0400

    Testing Auto-Update Feature

--- src/main.js
+++ src/main.js
@@ -1,55 +1,68 @@
-// main.js
-
-const { app, BrowserWindow, autoUpdater, ipcMain } = require('electron');
+const { app, BrowserWindow, autoUpdater, dialog, ipcMain } = require('electron');
 const log = require('electron-log');
 const path = require('path');
 const url = require('url');
 
+// Setting log level
 log.transports.file.level = 'info';
+
+// Correcting the path setting method
+app.on('ready', () => {
+  log.transports.file.resolvePath = () => path.join(app.getPath('logs'), 'log.log');
+});
+
+// Redirecting console outputs to log file
 console.log = log.log;
-log.transports.file.file = '/Users/joshuameza/Library/Logs/Rasterizer/rasterizer.log';
+console.error = log.error;
+console.warn = log.warn;
+console.info = log.info;
 
-
+// Setting autoUpdater's logging to use electron-log
 autoUpdater.logger = log;
+autoUpdater.logger.transports.file.level = 'info';
 
-
+// Handling the 'update-downloaded' event
 autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
-  const dialogOpts = {
-    type: 'info',
-    buttons: ['Restart', 'Later'],
-    title: 'Application Update',
-    message: process.platform === 'win32' ? releaseNotes : releaseName,
-    detail: 'A new version has been downloaded. Restart the application to apply the updates.'
-  };
+    const dialogOpts = {
+        type: 'info',
+        buttons: ['Restart', 'Later'],
+        title: 'Application Update',
+        message: process.platform === 'win32' ? releaseNotes : releaseName,
+        detail: 'A new version has been downloaded. Restart the application to apply the updates.'
+    };
 
-  dialog.showMessageBox(dialogOpts).then((returnValue) => {
-    if (returnValue.response === 0) autoUpdater.quitAndInstall();
-  });
+    dialog.showMessageBox(dialogOpts).then((returnValue) => {
+        if (returnValue.response === 0) autoUpdater.quitAndInstall();
+    });
 });
 
 function createWindow() {
-    let mainWindow = new BrowserWindow({width: 800, height: 600});
-    mainWindow.loadFile('src/index.html');
-  
-    mainWindow.on('closed', function () {
-      mainWindow = null;
+    let mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false  // Consider security implications depending on your app's needs
+        }
     });
-  }
-  
-  app.on('ready', () => {
-    if (process.env.NODE_ENV === 'production') {
-      autoUpdater.checkForUpdates();
-    }
-  });
-  
-  app.on('window-all-closed', () => {
+    
+    mainWindow.loadFile('src/index.html');
+    
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
-      app.quit();
+        app.quit();
     }
-  });
-  
-  app.on('activate', () => {
+});
+
+app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
+        createWindow();
     }
-  });
+});
