69ffee150d39f6ec5a3a8faf4a97846c3db72d4b
Joshua Meza <joshuameza1@gmail.com>
2024-04-19 11:38:08 -0400

    Fixed logging issues and prepared for v0.0.7 release

--- src/main.js
+++ src/main.js
@@ -8,21 +8,30 @@ log.transports.file.level = 'info';
 
 // Correcting the path setting method
 app.on('ready', () => {
-  log.transports.file.resolvePath = () => path.join(app.getPath('logs'), 'log.log');
+  // Set path explicitly (optional, only if needed)
+log.transports.file.resolvePath = () => require('path').join(require('electron').app.getPath('userData'), 'log.log');
+
 });
 
-// Redirecting console outputs to log file
+// Redirect console output to log file
 console.log = log.log;
 console.error = log.error;
 console.warn = log.warn;
 console.info = log.info;
+
+
+log.info('Application starting...');
+log.warn('This is a warning message');
+log.error('This is an error message');
+log.info('Logs are being stored in:', log.transports.file.getFile().path);
+
 
 // Setting autoUpdater's logging to use electron-log
 autoUpdater.logger = log;
 autoUpdater.logger.transports.file.level = 'info';
 
 // Handling the 'update-downloaded' event
-autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
+autoUpdater.on('update-downloaded', (event, releaseNotesreleaseName) => {
     const dialogOpts = {
         type: 'info',
         buttons: ['Restart', 'Later'],
